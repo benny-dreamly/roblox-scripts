39a3419dc0212a4881b1bad0f977bf3c135bfb3b
linuxnoob121 <78334662+linuxnoob121@users.noreply.github.com>
2021-06-06 19:11:12 -0600

    Create badge-awarder.lua

---
+++ badge-awarder.lua
@@ -0,0 +1,9 @@
+local BadgeService = game:GetService("BadgeService")
+local badge1 =
+local badge2 = 
+
+game.Players.PlayerAdded:Connect(function(player)
+local playerUID = player.UserId
+BadgeService:AwardBadge(playerUID, badge1)
+
+end)
