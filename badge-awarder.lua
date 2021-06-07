0945925c6724f7cf3769e6fc1238e3813d7a49fd
linuxnoob121 <78334662+linuxnoob121@users.noreply.github.com>
2021-06-06 19:13:24 -0600

    Update badge-awarder.lua

--- badge-awarder.lua
+++ badge-awarder.lua
@@ -3,7 +3,8 @@ local badge1 =
 local badge2 = 
 
 game.Players.PlayerAdded:Connect(function(player)
-local playerUID = player.UserId
-BadgeService:AwardBadge(playerUID, badge1)
+
+    local playerUID = player.UserId
+    BadgeService:AwardBadge(playerUID, badge1)
 
 end)
